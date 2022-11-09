cb1933cf8b95f8f104f5c41544db56122ed542d6
Abhishek <abhishekgupta954@gmail.com>
2022-11-09 19:09:15 +0530

    Signout fIX

--- client/src/utils/supportfunctions.jsx
+++ client/src/utils/supportfunctions.jsx
@@ -12,7 +12,7 @@ export const filterByLanguage = [
   { id: 1, name: "Tamil", value: "tamil" },
   { id: 2, name: "English", value: "english" },
   { id: 3, name: "Malayalam", value: "malayalam" },
-  { id: 4, name: "Telungu", value: "Telungu" },
+  { id: 4, name: "Punjabi", value: "punjabi" },
   { id: 5, name: "Hindi", value: "hindi" },
 ];
 
